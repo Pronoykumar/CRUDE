52f99f0dbd66e60b5295e352526c6e83c8cf326e
Pronoy Roy <pronoy.roy083@gmail.com>
2016-05-15 18:23:03 +0600

    Multipal chack box

--- index.php
+++ index.php
@@ -10,11 +10,33 @@ and open the template in the editor.
         <link rel="stylesheet" type="text/css" href="tamplate/css/font-awesome.min.css">
         <link rel="stylesheet" type="text/css" href="tamplate/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="tamplate/css/style.css">
-        <title></title>
+        <title>CRUDE System</title>
+        <style>
+            .top{
+                text-align: center;
+                font-size: 20px;
+                margin: 10px auto;
+                padding: 50px;
+                width: 200px;
+                height: auto;
+                border: 1px solid #23527c;
+                border-radius: 5px;
+                background-color: #c1e2b3;
+            }
+            .top a{
+                text-decoration: none;
+            }
+        </style>
     </head>
     <body>
-        <a href="views/Mobile/create.php">Mobile</a>
-        <a href="views/Hobbies/create.php">Mobile</a>
+        
+        <div class="top">
+        <a href="views/Mobile/create.php">Mobile</a><br>
+        <a href="views/Hobbies/create.php">Hobbies</a>
+        </div>
+        
+        
+        
         <?php
         // put your code here
         ?>
